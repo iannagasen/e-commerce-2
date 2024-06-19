524c849d5fc3a4d084f39548f00bb7112bbc0a3c
iann.agasen <iann.agasen@gmail.com>
2024-06-20 07:59:51 +0800

    Add inventory update entities and integration tests

--- src/backend/microservices/inventory-service/src/test/java/dev/agasen/ecom/inventory/InventoryServiceIntegrationTest.java
+++ src/backend/microservices/inventory-service/src/test/java/dev/agasen/ecom/inventory/InventoryServiceIntegrationTest.java
@@ -13,6 +13,7 @@ import dev.agasen.ecom.api.core.inventory.model.InventoryDeductionRequest;
 import dev.agasen.ecom.api.core.inventory.model.InventoryUpdateType;
 import dev.agasen.ecom.inventory.repository.InventoryEntity;
 import dev.agasen.ecom.inventory.repository.InventoryRepository;
+import dev.agasen.ecom.inventory.repository.InventoryUpdateEntity;
 import dev.agasen.ecom.inventory.repository.InventoryUpdateRepository;
 import dev.agasen.ecom.inventory.service.InventoryService;
 import reactor.core.publisher.Mono;
@@ -51,6 +52,23 @@ public class InventoryServiceIntegrationTest extends MongoDBTestBase {
 
     inventoryRepository.saveAll(List.of(inv1, inv2, inv3)).collectList().block();
 
+    var inv2_update1 = InventoryUpdateEntity.builder()
+      .updateId(1L)
+      .inventoryId(2L)
+      .orderId(10L)
+      .type(InventoryUpdateType.PURCHASE)
+      .quantity(5)
+      .build();
+
+    var inv2_update2 = InventoryUpdateEntity.builder()
+      .updateId(2L)
+      .inventoryId(2L)
+      .orderId(20L)
+      .type(InventoryUpdateType.PURCHASE)
+      .quantity(6)
+      .build();
+
+    updateRepository.saveAll(List.of(inv2_update1, inv2_update2)).collectList().block();
   }
 
     
@@ -72,7 +90,29 @@ public class InventoryServiceIntegrationTest extends MongoDBTestBase {
         assertEquals(1L, inv.getProductId(), 0);
         assertEquals(1, inv.getHistory().size());
         assertEquals(InventoryUpdateType.PURCHASE, inv.getLastUpdate().getType());
+        assertEquals(1L, inv.getLastUpdate().getOrderId(), 0);
         assertEquals(5, inv.getLastUpdate().getQuantity());
+      })
+      .verifyComplete();
+
+    var deduct_6_fromStockof20_productId_2L = InventoryDeductionRequest.builder()
+      .productId(2L)
+      .orderId(2L)
+      .customerId(1L)
+      .quantity(6)
+      .build();
+
+    Mono<InventoryEntity> deductMono2 = inventoryService.deduct(deduct_6_fromStockof20_productId_2L);
+
+    StepVerifier.create(deductMono2)
+      .assertNext(inv -> {
+        assertEquals(14, inv.getStock(), 0);
+        assertEquals(2L, inv.getInventoryId(), 0);
+        assertEquals(2L, inv.getProductId(), 0);
+        assertEquals(2 + 1, inv.getHistory().size()); // 2 existing + 1 new
+        assertEquals(InventoryUpdateType.PURCHASE, inv.getLastUpdate().getType());
+        assertEquals(2L, inv.getLastUpdate().getOrderId(), 0);
+        assertEquals(6, inv.getLastUpdate().getQuantity());
       })
       .verifyComplete();
   }
