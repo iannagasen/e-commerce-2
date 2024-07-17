0ef6ef9959653fee3de2398afe989d3beed96455
iann.agasen <iann.agasen@gmail.com>
2024-07-17 18:03:26 +0800

    Create generic Global Exception Handler

--- src/backend/util/src/main/java/dev/agasen/ecom/util/http/GlobalControllerExceptionHandler.java
+++ src/backend/util/src/main/java/dev/agasen/ecom/util/http/GlobalControllerExceptionHandler.java
@@ -3,6 +3,11 @@ package dev.agasen.ecom.util.http;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
@@ -27,13 +32,22 @@ class GlobalControllerExceptionHandler {
 //     return createHttpErrorInfo(UNPROCESSABLE_ENTITY, request, ex);
 //   }
 
-//   private HttpErrorInfo createHttpErrorInfo(
-//     HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  @ExceptionHandler(Exception.class)
+  public @ResponseBody HttpErrorInfo handleNotFoundExceptions(
+    ServerHttpRequest request, Exception ex) {
 
-//     final String path = request.getPath().pathWithinApplication().value();
-//     final String message = ex.getMessage();
+    return createHttpErrorInfo(HttpStatus.INTERNAL_SERVER_ERROR, request, ex);
+  }
+  
 
-//     LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
-//     return new HttpErrorInfo(httpStatus, path, message);
-//   }
+  private HttpErrorInfo createHttpErrorInfo(
+    HttpStatus httpStatus, ServerHttpRequest request, Exception ex) {
+
+    final String path = request.getPath().pathWithinApplication().value();
+    final String message = ex.getMessage();
+
+    LOG.debug("Returning HTTP status: {} for path: {}, message: {}", httpStatus, path, message);
+    return new HttpErrorInfo(httpStatus, path, message);
+  }
 }
