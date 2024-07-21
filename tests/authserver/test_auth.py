4bb12abc65b34f0979aff5a46055437150294810
iann.agasen <iann.agasen@gmail.com>
2024-07-21 17:44:13 +0800

    Python tests for authentication server

--- tests/authserver/test_auth.py
+++ tests/authserver/test_auth.py
@@ -1,53 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 import base64
+from _helpers import extract_code_from_url
 
-# 
 # pip install requests beautifulsoup4
-# 
 
-def extract_code_from_url(url):
-  # Find the start index of the 'code' parameter
-  code_start_index = url.find('code=')
-  if code_start_index != -1:
-    # Find the end index of the 'code' parameter
-    code_start_index += len('code=')
-    code_end_index = url.find('&', code_start_index)
-    
-    # If no '&' is found, the code is till the end of the URL
-    if code_end_index == -1:  
-      code_end_index = len(url)
-    
-    # Extract the 'code' parameter value
-    return url[code_start_index:code_end_index]
-  else:
-    return None
-  
+client = "client"
+secret = "secret"
+redirect_uri = "my.redirect.uri"
+username = "ian"
+password = "pass"
+
 # Define the endpoint URL
 endpoint = "http://localhost:8080/oauth2/authorize?" \
   "response_type=code&" \
-  "client_id=client&" \
+  f"client_id={client}&" \
   "scope=openid&" \
-  "redirect_uri=my.redirect.uri"
+  f"redirect_uri={redirect_uri}"
 
 # Create a session object to handle cookies
 session = requests.Session()
 
 # Make a GET request to the endpoint
-print("Making a GET request to the endpoint")
+print("Step 1: Making a GET request to the endpoint")
 response = session.get(endpoint, allow_redirects=True)
+print("---------------------------------------------")
 
 # Save the response content to a file
 # with open('login_page.html', 'w') as file:
 #     file.write(response.text)
 
 # Extract the CSRF token from the login page
-print("Extracting the CSRF token from the login page")
+print("Step 2: Extracting the CSRF token from the login page")
 soup = BeautifulSoup(response.text, 'html.parser')
 csrf_token = None
 csrf_input = soup.find('input', {'name': '_csrf'})
 if csrf_input:
-    csrf_token = csrf_input.get('value')
+  csrf_token = csrf_input.get('value')
 
 print(f"CSRF token: {csrf_token}")
 
@@ -61,12 +50,12 @@ print("---------------------------------------------")
 # Submit the login form
 login_url = "http://localhost:8080/login"
 login_data = {
-    'username': 'ian',
-    'password': 'pass',
+    'username': username,
+    'password': password,
     '_csrf': csrf_token
 }
 
-print("Submitting the login form")
+print("Step 3: Submitting the login form")
 login_response = session.post(login_url, data=login_data)
 
 # Save the response content to a file
@@ -88,15 +77,15 @@ print(f"Extracted code: {code}")
 
 # Exchange the authorization code for an access token
 # Convert client secret to base64
-http_basic_auth = 'client:secret'
+http_basic_auth = f"{client}:{secret}"
 http_basic_auth_base64 = base64.b64encode(http_basic_auth.encode()).decode()
 
 # Make the POST request with the modified code
 token_response = session.post("http://localhost:8080/oauth2/token", data={
   'grant_type': 'authorization_code',
   'code': code,
-  'redirect_uri': 'my.redirect.uri',
-  'client_id': 'client',
+  'redirect_uri': redirect_uri,
+  'client_id': client,
 }, headers={'Authorization': f'Basic {http_basic_auth_base64}'})
 
 print("Token response:")
