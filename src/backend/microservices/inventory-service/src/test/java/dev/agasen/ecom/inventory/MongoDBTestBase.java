f228b8a251a1d6494e92b902299eeef9eb4825eb
iann.agasen <iann.agasen@gmail.com>
2024-08-19 19:17:54 +0800

    fix mongodb auth issue that was previously working

--- src/backend/microservices/inventory-service/src/test/java/dev/agasen/ecom/inventory/MongoDBTestBase.java
+++ src/backend/microservices/inventory-service/src/test/java/dev/agasen/ecom/inventory/MongoDBTestBase.java
@@ -2,20 +2,26 @@ package dev.agasen.ecom.inventory;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.junit.jupiter.Container;
 
-public class MongoDBTestBase {
+public abstract class MongoDBTestBase {
   private static final String MONGODB_VER = "mongo:6.0.4";
-  protected static final MongoDBContainer MONGODB = new MongoDBContainer(MONGODB_VER);
-
-  static {
-    MONGODB.start();
-  }
+  
+  @Container
+  protected static GenericContainer<?> MONGODB = new GenericContainer<>("mongo:6.0.4")
+  .withEnv("MONGO_INITDB_ROOT_USERNAME", "admin")
+  .withEnv("MONGO_INITDB_ROOT_PASSWORD", "password")
+  .withEnv("MONGO_INITDB_DATABASE", "inventory-db")
+  .waitingFor(Wait.forLogMessage("(?i).*Waiting for connections*.*", 1))
+  .withExposedPorts(27017); 
 
   @DynamicPropertySource
   static void setProperties(DynamicPropertyRegistry registry) {
-    registry.add("spring.data.mongodb.host", MONGODB::getContainerIpAddress);
-    registry.add("spring.data.mongodb.port", () -> MONGODB.getMappedPort(27017));
-    registry.add("spring.data.mongodb.database", () -> "test");
+    MONGODB.start();
+    registry.add("spring.data.mongodb.host", MONGODB::getHost);
+    registry.add("spring.data.mongodb.port", MONGODB::getFirstMappedPort);
   }  
 }
